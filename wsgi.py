20c23ae35fdbb10797bf6af41db2f5125407b50e
automan <automan@192.168.8.155>
2021-10-29 10:35:24 +0800

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -2,16 +2,10 @@
 #gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
 #!/usr/bin/env python
 import os
-from app import create_app, db
-from app.models import User, Role, Catalog
+from app import create_app
 from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
-from flask_uploads import configure_uploads, patch_request_class
-from app import images
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-configure_uploads(app, images)
-patch_request_class(app)
 
 # server = Server(host="0.0.0.0", port=5000 , debug = True, ssl_context=context) test github
 def make_shell_context():
@@ -19,3 +13,4 @@ def make_shell_context():
 
 if __name__ == '__main__':
     app.run()
+~                      
