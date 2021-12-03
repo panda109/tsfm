1dac78cf004d863389518775ba784c9bcacf11a3
chliuhelmut <63987823+chliuhelmut@users.noreply.github.com>
2021-12-03 14:30:18 +0800

    remove unused  app.models import

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@
 import os
 from app import create_app, db
 from flask_script import Manager, Shell, Server
-from app.models import User
+#from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 from werkzeug.security import generate_password_hash
 from sqlalchemy.orm.dynamic import CollectionHistory
