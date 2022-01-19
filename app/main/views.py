d31ea0a036d1da1289f26dd49b869f5aeca07891
automan <automan@192.168.7.92>
2022-01-19 16:46:27 +0800

    update view

--- app/main/views.py
+++ app/main/views.py
@@ -18,8 +18,13 @@ def publish_hello():
     
     token = request.args.get('token')
     userid = request.args.get('userid')
-    #userid = '94f43ded-55b6-4354-aaae-c5cc20fde280'
     redirect_app = request.args.get('redirect_app')
+    
+    #token = ""
+    #userid = '94f43ded-55b6-4354-aaae-c5cc20fde280'
+    #redirect_app = ""
+    
+    
     devicelist = Device_Info.get_by_userid(userid)
     user_info = User_Mgmt.query.filter_by(user_id=userid)
     return render_template('index.html',userid = userid,redirect_app=redirect_app ,user_info = user_info , devices = devicelist)
