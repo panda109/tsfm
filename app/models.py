cbfd2ada3d07d9bb2588db7c203b8faa9df1fa79
automan <automan@localhost>
2022-04-07 15:00:23 +0800

    multi_lan and requirement

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 
 from flask import current_app
 from flask_login import UserMixin
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+#from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
