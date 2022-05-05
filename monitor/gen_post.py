e4b426bccef214bed82f03a30e60e195d0a786e9
shawn <shawn@localhost>
2022-05-05 21:46:41 +0800

    Change URL to https

--- monitor/gen_post.py
+++ monitor/gen_post.py
@@ -26,14 +26,14 @@ class GenPosts(object):
         dicNotify = {
             "format": 1,
             "name": "NextDrive 太陽能發電監控服務",
-            "icon": "http://%s/postpicture/SolarPanel.png" % self.strHost,
+            "icon": "https://%s/postpicture/SolarPanel.png" % self.strHost,
             "title": self.dicMsgs['PST_01_01'],
             # "太陽能發電模組 %s，今日 %s ~ %s 發電量： %s kWh。低於 %s kWh 的 %s%。"
             "description": self.dicMsgs['PST_01_02'] % (strPVName, strStartTime, strEndTime, intTotal, intTargetTotal, intTargetPercent),
             "images": [
                     {
-                        "previewImageUrl": "http://%s/postpicture/post_LowSellingPower.png" % self.strHost,
-                        "originalContentUrl": "http://%s/postpicture/post_LowSellingPower.png" % self.strHost
+                        "previewImageUrl": "https://%s/postpicture/post_LowSellingPower.png" % self.strHost,
+                        "originalContentUrl": "https://%s/postpicture/post_LowSellingPower.png" % self.strHost
                     }
                 ],
             "contents": []
@@ -44,14 +44,14 @@ class GenPosts(object):
         dicNotify = {
             "format": 1,
             "name": "NextDrive 太陽能發電監控服務",
-            "icon": "http://%s/postpicture/SolarPanel.png" % self.strHost,
+            "icon": "https://%s/postpicture/SolarPanel.png" % self.strHost,
             "title": self.dicMsgs['PST_02_01'],
             # 太陽能發電模組 %s，過去 %s 分鐘內發電狀況異常，請檢查設備狀態。
             "description": self.dicMsgs['PST_02_02'] % (strPVName, strMin),
             "images": [
                     {
-                        "previewImageUrl": "http://%s/postpicture/post_NoPower.png" % self.strHost,
-                        "originalContentUrl": "http://%s/postpicture/post_NoPower.png" % self.strHost
+                        "previewImageUrl": "https://%s/postpicture/post_NoPower.png" % self.strHost,
+                        "originalContentUrl": "https://%s/postpicture/post_NoPower.png" % self.strHost
                     }
                 ],
             "contents": []
@@ -62,14 +62,14 @@ class GenPosts(object):
         dicNotify = {
             "format": 1,
             "name": "NextDrive 太陽能發電監控服務",
-            "icon": "http://%s/postpicture/SolarPanel.png" % self.strHost,
+            "icon": "https://%s/postpicture/SolarPanel.png" % self.strHost,
             "title": self.dicMsgs['PST_03_01'],
             # 群組 %s 中，過去 %s 分鐘內有部份太陽能發電模組發電狀況異常，請檢查設備狀態。
             "description": self.dicMsgs['PST_03_02'] % (strGroup, strMin),
             "images": [
                     {
-                        "previewImageUrl": "http://%s/postpicture/post_LowSellingPower.png" % self.strHost,
-                        "originalContentUrl": "http://%s/postpicture/post_LowSellingPower.png" % self.strHost
+                        "previewImageUrl": "https://%s/postpicture/post_LowSellingPower.png" % self.strHost,
+                        "originalContentUrl": "https://%s/postpicture/post_LowSellingPower.png" % self.strHost
                     }
                 ],
             "contents": []
