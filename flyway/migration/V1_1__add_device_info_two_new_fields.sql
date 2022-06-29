5876ccc94af8ee592563de2e9d3a16e41ce2a553
tim <yr6703@yahoo.com.tw>
2022-06-29 17:04:18 +0800

    update sql
    for flyway

--- flyway/migration/V1_1__add_device_info_two_new_fields.sql
+++ flyway/migration/V1_1__add_device_info_two_new_fields.sql
@@ -1,3 +1,3 @@
 ALTER TABLE public.device_info
-ADD COLUMN IF NOT EXISTS today_energy_amount FLOAT8 DEFAULT 0
+ADD COLUMN IF NOT EXISTS today_energy_amount FLOAT8 DEFAULT 0,
 ADD COLUMN IF NOT EXISTS yesterday_energy_amount FLOAT8 DEFAULT 0;
